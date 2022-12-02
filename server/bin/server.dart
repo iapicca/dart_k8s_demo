50ed08f3e8647702c519d5f240eeb168e9e25eff
Francesco Iapicca <iapicca@gmail.com>
2022-12-02 10:09:48 +0200

    'hello world' server

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -1,5 +1,18 @@
-import 'package:server/server.dart' as server;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${server.calculate()}!');
+const name = 'server';
+const port = 8888;
+
+void main(List<String> arguments) async {
+  print('listening on $port');
+  final server = await HttpServer.bind(InternetAddress.anyIPv4, port);
+
+  await for (final request in server) {
+    request.response
+      ..headers.set('Server', name)
+      ..headers.set('Content-Type', 'text/plain')
+      ..write('hello world');
+
+    await request.response.close();
+  }
 }
